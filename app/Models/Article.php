a8032ea03e08bc46b7491beea741d4bde6e30536
JhonCode <jjv20618@gmail.com>
2024-08-22 20:29:35 -0600

    Setting and seeding database
    I modified the models classes to allow the relationships between the tables and i added two seeders to insert important data in the tables.

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function images(){
+        return $this->belongsToMany(Image::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        //Creación de usuario por defecto.
+        // $user = new User();
+        // $user->name = "Jhon";
+        // $user->email = "jjv20618@gmail.com";
+        // $user->password = "07092002fake";
+        // $user->save();
+
+        //$this->call(PermissionsTableSeeder::class);
+        //$this->call(RolesTableSeeder::class);
     }
 }
